7f8e7eaf2e4664da877dbcd5afc3b1d6bc6dfff3
USUARIO <USUARIO@PC30>
2018-09-19 21:40:51 -0500

    Conexion a la Base de Datos MYSQL

---
+++ src/conexionmysqlcorba/Conexion.java
@@ -0,0 +1,17 @@
+package conexionmysqlcorba;
+import java.sql.*;
+
+public class Conexion{
+    public Connection conex;
+    
+    public Connection conectar(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conex = DriverManager.getConnection("jdbc:mysql://localhost/ConexionMySqlCorba","root","");
+            System.out.println("Conexion establecida");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return conex;
+    }
+}
---
+++ src/conexionmysqlcorba/Prueba.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexionmysqlcorba;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Prueba {
+    public static void main(String[] args) {
+        
+        Conexion conext = new Conexion();
+        
+        
+        conext.conectar();
+    }
+}
