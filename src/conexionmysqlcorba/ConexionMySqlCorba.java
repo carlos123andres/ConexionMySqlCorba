56c510b98c161a84b775e9807849706cb010fb77
USUARIO <USUARIO@PC30>
2018-09-19 21:01:11 -0500

    Inicio del Proyecto

--- src/conexionmysqlcorba/ConexionMySqlCorba.java
+++ src/conexionmysqlcorba/ConexionMySqlCorba.java
@@ -15,7 +15,6 @@ public class ConexionMySqlCorba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    
+    /*Inicio del Proyecto*/
     }
-    
 }
