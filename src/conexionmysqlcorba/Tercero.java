ef0496ac0654c0e260ee5b5974392f2ddacf9b68
USUARIO <USUARIO@PC20>
2018-09-26 21:36:44 -0500

    Se crea la clase Tercero que va implementar los metodos del CRUD.

--- src/conexionmysqlcorba/Tercero.java
+++ src/conexionmysqlcorba/Tercero.java
@@ -1,5 +1,40 @@
 package conexionmysqlcorba;
+
 import java.sql.*;
-public class Tercero {
+import Terceros.*;
+
+
+
+public class Tercero extends TerceroPOA {
     
+    
+    
+    Conexion objConec=new Conexion();
+
+    @Override
+    public boolean insertarTercero(String nombre, String apellidos, String telefono) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean actualizarTercero(int id, String nombre, String apellidos, String telefono) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean eliminarTercero(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean consultarTercero(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void shutdwn() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+            
+            
 }
