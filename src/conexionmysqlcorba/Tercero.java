78b21a5c9102dd5cc290f3a6ca247865db8c5309
Pastrana <>
2018-10-19 23:01:28 -0500

    Se Implementa el metodo consultarTercero.

--- src/conexionmysqlcorba/Tercero.java
+++ src/conexionmysqlcorba/Tercero.java
@@ -2,6 +2,7 @@ package conexionmysqlcorba;
 
 import java.sql.*;
 import Terceros.*;
+import javax.swing.JOptionPane;
 
 
 
@@ -13,23 +14,104 @@ public class Tercero extends TerceroPOA {
 
     @Override
     public boolean insertarTercero(String nombre, String apellidos, String telefono) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        boolean resultado = false;
+        
+        try {
+            String sql= "insert into terceros (nombres,apellidos,telefono)values('"+nombre+"','"+apellidos+"','"+telefono+"')";
+            objConec.conectar();
+            Statement st = objConec.conex.createStatement();
+            int valor = st.executeUpdate(sql);
+            if (valor>0){
+                resultado = true;
+            }
+            objConec.conex.close();
+            st.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al insertar. "+e.getMessage());
+        }
+        return resultado;
+        
+        
     }
 
     @Override
     public boolean actualizarTercero(int id, String nombre, String apellidos, String telefono) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean resultado = false;
+        
+        try {
+            String sql = "update tercero set nombres = '"+nombre+"',apellidos = '"+apellidos+"',telefono = '"+telefono+"' where id = '"+id+"'";
+           
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al insertar. "+e.getMessage());
+        }
+        return resultado;
     }
 
     @Override
     public boolean eliminarTercero(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        boolean resultado = false;
+        
+        try {
+            String sql = "Delete from terceros where id = "+id;
+            objConec.conectar();
+            Statement st = objConec.conex.createStatement();
+            int valor = st.executeUpdate(sql);
+            if (valor>0){
+                resultado = true;
+            }
+            objConec.conex.close();
+            st.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al insertar. "+e.getMessage());
+        }
+
+        return resultado;
+        
     }
 
     @Override
     public boolean consultarTercero(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        String resultado = "";
+        
+            try {
+                String sqlConsultar = "Select * from terceros where id = "+ id;
+                objConec.conectar();
+                Statement st = objConec.conex.createStatement();
+                ResultSet rs = st.executeQuery(sqlConsultar);
+                    while (rs.next()) {
+                        resultado += rs.getString(2) + " - "
+                        + rs.getString(3) + " - "
+                        + rs.getString(4);
+            }
+            //Se cierran las conexiones
+            rs.close();
+            objConec.conex.close();
+        } catch (Exception e) {
+              //  System.out.println("Error: "+ e.getMessage);
+        }
+        
+        return true;
+        
     }
+    
+    
+    public ResultSet cargarTercero(){
+        ResultSet resultado = null;
+        try {
+            String sql = "Select nombres, apellidos, telefono from terceros";
+            objConec.conectar();
+            Statement st = objConec.conex.createStatement();
+            resultado = st.executeQuery(sql);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Error: "+ e.getMessage());
+        }
+        return resultado;
+    }
+    
+    
 
     @Override
     public void shutdwn() {
